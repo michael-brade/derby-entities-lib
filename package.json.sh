0a258b7c2c543419b2dbd1620cb971d5201bf7a7
Michael Brade <brade@kde.org>
2015-10-09 12:56:33 -0700

    these changes absolutely warrant a new release; replace package.json with a script

---
+++ package.json.sh
@@ -0,0 +1,45 @@
+#!/usr/local/bin/lsc -cj
+
+name: 'derby-entities-lib'
+description: 'Base library for derby-entity CRUD component and derby-entities-visualizations'
+version: '1.1.0'
+
+main: 'api.ls'
+
+author:
+    name: 'Michael Brade'
+    email: 'brade@kde.org'
+
+keywords:
+    'derby'
+    'entity'
+    'crud'
+
+
+repository:
+    type: 'git'
+    url: 'michael-brade/derby-entity'
+
+dependencies:
+    # utils
+    'lodash': '3.x'
+
+devDependencies:
+    'livescript': '1.x'
+
+    # possibly, depending on how you set it up
+    'browserify-livescript': '0.2.x'
+
+
+scripts:
+    test: "echo \"TODO: no tests specified yet\" && exit 1"
+
+engines:
+    node: '4.x'
+
+license: 'MIT'
+
+bugs:
+    url: 'https://github.com/michael-brade/derby-entities-lib/issues'
+
+homepage: 'https://github.com/michael-brade/derby-entities-lib#readme'
