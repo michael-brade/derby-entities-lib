0d32bb49fdefce5b1505b14dff33817bb444a6be
Michael Brade <brade@kde.org>
2015-12-23 22:18:58 -0800

    got rid of gulp completely; put package.json in git
    Now it will be possible to install a debugging version using
    
       npm install michael-brade/derby-entities-lib

--- package.json.sh
+++ package.json.sh
@@ -15,7 +15,6 @@ keywords:
     'entity'
     'crud'
 
-
 repository:
     type: 'git'
     url: 'michael-brade/derby-entities-lib'
@@ -25,35 +24,54 @@ dependencies:
     'lodash': '3.x'
 
 devDependencies:
-    'livescript': "1.4"
-    'gulp': "3.x"
-    'gulp-plumber': "1.x"
-    'gulp-livescript': "3.x"
-    'gulp-uglify': "1.x"
-    'gulp-notify': "2.x"
-    'gulp-gitignore': "*"
-
-
+    'livescript': "1.4.x"
+    'uglify-js': '2.6.x'
 
 scripts:
     ## building
 
     # make sure a stash will be created and stash everything not committed
     # beware: --all would be really correct, but it also removes node_modules, so use --include-untracked instead
-    prebuild: 'touch .create_stash && git stash save --include-untracked "npm build stash"'
-    # build the distribution
-    build: "gulp build && ./package.json.sh && cp package.json dist"
-    # restore the original situation
-    postbuild: 'git stash pop --index && rm .create_stash'
+    prebuild: 'npm run clean; touch .create_stash && git stash save --include-untracked "npm build stash";'
 
-    clean: "rm -rf dist"
+    # build the distribution under dist: create directory structure, compile to JavaScript, uglify
+    build: "
+        export DEST=dist;
+        export ASSETS='.*\.css|.*\.html|./README\.md|./package\.json';
+
+        find -path './node_modules*' -prune -o -name '*.ls' -print0
+        | xargs -n1 -0 sh -c '
+            echo Compiling and minifying $0...;
+            DEST_PATH=\"$DEST/`dirname $0`\";
+            mkdir -p \"$DEST_PATH\";
+            lsc -cp \"$0\" | uglifyjs - -cm -o \"$DEST_PATH/`basename -s .ls \"$0\"`\".js;
+        ';
+
+        echo \"\033[01;32mCopying assets...\033[00m\";
+        find \\( -path './node_modules*' -o -path \"./$DEST/*\" \\) -prune -o -regextype posix-egrep -regex $ASSETS -print0
+        | xargs -n1 -0 sh -c '
+            mkdir -p \"$DEST/`dirname \"$0\"`\";
+            cp -a \"$0\" \"$DEST/$0\"
+        ';
+
+        echo \"\033[01;32mDone!\033[00m\";
+    "
+    # restore the original situation
+    postbuild: 'git stash pop --index && rm .create_stash;'
+
+    clean: "rm -rf dist;"   # the ; at the end is very important! otherwise "npm run clean ." would delete everything
 
     ## testing
 
-    test: "echo \"TODO: no tests specified yet\" && exit 1"
+    test: "echo \"TODO: no tests specified yet\" && exit 1;"
+
+    ## publishing - run as "npm run publish"
+
+    prepublish: "npm run clean; npm run build;"
+    publish: "npm publish dist;"
 
 engines:
-    node: '4.x'
+    node: '4.x || 5.x'
 
 license: 'MIT'
 
